6256fc88c3631360eeb233a9920bb9f115985a67
SayyedMAshah <maaztg@gmail.com>
2025-09-02 19:37:26 +0100

    Fix Vercel build: Remove Prisma dependencies causing PostgreSQL conflicts and exclude pg modules from webpack

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config, { isServer }) => {
+    // Exclude PostgreSQL modules that we don't use
+    config.externals = config.externals || [];
+    if (isServer) {
+      config.externals.push('pg', 'pg-hstore', 'pg-native');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
